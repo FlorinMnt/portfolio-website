47fbd0ae8515c53816297ba20dbd2ccda35237c0
Florin <>
2020-08-04 15:01:47 +0300

    added smooth scroll

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navLinks');
+    const navLinks = document.querySelectorAll('.navLinks li ')
+
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Link
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + 0.6}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle('toggle')
+    });
 }
+navSlide();
