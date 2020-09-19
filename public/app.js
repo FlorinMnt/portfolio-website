7e7593c0cf8e5f413bffdc7203a53b5bb44b4dee
Florin <>
2020-09-19 15:44:29 +0300

    added colors

--- public/app.js
+++ public/app.js
@@ -37,25 +37,34 @@ const navScroll = document.querySelector('nav');
 let prevScrollPos = window.pageYOffset;
 
 window.addEventListener('scroll', () => {
-        let currentScrollPos = window.pageYOffset;
-        if (prevScrollPos < currentScrollPos) {
-            navScroll.classList.add('hide');
-        } else {
-            navScroll.classList.remove('hide');
-        }
-        prevScrollPos = currentScrollPos;
-    })
-    // Your web app's Firebase configuration
-    /* var firebaseConfig = {
-        apiKey: "AIzaSyCl3qbX7QbRFB5ee8-08wLz4lPF5fQcUlA",
-        authDomain: "munteanu-florin-web-app.firebaseapp.com",
-        databaseURL: "https://munteanu-florin-web-app.firebaseio.com",
-        projectId: "munteanu-florin-web-app",
-        storageBucket: "munteanu-florin-web-app.appspot.com",
-        messagingSenderId: "1052825525646",
-        appId: "1:1052825525646:web:e643a17ef2662a665198df",
-        measurementId: "G-2HZGE1Q03V"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    firebase.analytics(); */
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollPos < currentScrollPos) {
+        navScroll.classList.add('hide');
+    } else {
+        navScroll.classList.remove('hide');
+    }
+    prevScrollPos = currentScrollPos;
+});
+
+
+
+
+
+
+
+
+
+// Your web app's Firebase configuration
+/* var firebaseConfig = {
+    apiKey: "AIzaSyCl3qbX7QbRFB5ee8-08wLz4lPF5fQcUlA",
+    authDomain: "munteanu-florin-web-app.firebaseapp.com",
+    databaseURL: "https://munteanu-florin-web-app.firebaseio.com",
+    projectId: "munteanu-florin-web-app",
+    storageBucket: "munteanu-florin-web-app.appspot.com",
+    messagingSenderId: "1052825525646",
+    appId: "1:1052825525646:web:e643a17ef2662a665198df",
+    measurementId: "G-2HZGE1Q03V"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics(); */
